ab8d55f0d1328e0b5c2aa5532b6c6213ee01be4a
Brady Anderson <bradyanderson@Bradys-MBP.hsd1.in.comcast.net>
2018-10-28 22:44:54 -0400

    Add get command

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos}); //using object instead of array makes for more flexible coding options
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
